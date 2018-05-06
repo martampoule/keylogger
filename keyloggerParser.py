187d164df8a4ddaf4d195498a5953361e034ddfa
martampoule <marta.cattaneo.96@gmail.com>
2018-05-06 12:58:44 +0200

    keylogger e parser

--- keyloggerParser.py
+++ keyloggerParser.py
@@ -1,70 +1,106 @@
 #! /usr/local/bin/python3
 
+import os
 import sys
 import pprint
+import re
 
-def usage():
-	print("USAGE : python3 " + sys.argv[0] + " [FILE]")
-	exit(1)
+folder = os.path.dirname(os.path.realpath(__file__))
+
+t = "\t";
+n = "\n";
 
 def main():
+	file = folder + "/key.txt"
 	try:
-		file = open(sys.argv[1], 'r')
+		file = open(file, 'r')
+		# print file
 	except:
-		usage();
+		print ("some error");
 
-	result = {}
 
-	file.readline()
-	file.readline()
-	file.readline()
-	
-	date = file.readline()
-	file.readline()
-	
+
 	data = file.readline()
-	file.close()
-	# print(date, data)
 
-	beginSpecialKey = 0
 
-	k = -1
+	# copia e incolla le stringhe per rilevare eventuali altri tasti non presenti nella lista
+	# -------------
+	match_unknown = "[unknown]"
+	unknown = len(data.split(match_unknown)) - 1
 
-	betweenParens = False
-	for i in data:
-		k+=1
-		if(i == ']'):
-			betweenParens = False
-			try:
-				result[str(data[beginSpecialKey:k])] += 1
-			except:
-				result[str(data[beginSpecialKey:k])] = 1
-			beginSpecialKey = 0
-			continue
-		if(i == '['):
-			betweenParens = True
-			beginSpecialKey = k+1
-			continue
-		if(not(betweenParens)):
-			try:
-				result[str(i)] += 1
-			except:
-				result[str(i)] = 1
-		
+	match_del = "[del]"
+	_del = len(data.split(match_del)) - 1
 
-	# print(data
+	match_return = "[return]"
+	_return = len(data.split(match_return)) - 1
 
-	date = date.replace(' ', '').replace("'", '').replace(':', '').replace('\n','')
-	filename = date + ".txt"
+	match_left_cmd = "[left_cmd]"
+	left_cmd  = len(data.split(match_left_cmd)) - 1
+
+	match_right_cmd = "[right-cmd]"
+	right_cmd  = len(data.split(match_right_cmd)) - 1
+
+	match_left_shift = "[left-shift]"
+	left_shift  = len(data.split(match_left_shift)) - 1
+
+	match_left_option = "[left-option]"
+	left_option = len(data.split(match_left_option)) - 1
+
+	match_tab = "[tab]"
+	tab = len(data.split(match_tab)) - 1
+
+	match_up = "[up]"
+	up = len(data.split(match_up)) - 1
+
+	match_down = "[down]"
+	down = len(data.split(match_down)) - 1
+
+
+	# inserisci su replace_list eventuali altri tasti non presenti nella lista
+	# -------------
+	replace_list = [match_unknown,match_del,match_return,match_right_cmd,match_left_cmd,match_left_shift,match_left_option,match_tab,match_down];
+	txt = re.sub("|".join(map(re.escape, replace_list)), "", data)
+
+        
+##	filename = date.replace(' ', '').replace("'", '').replace(':', '').replace('\n','') + ".txt"
+##	print (filename)
+	filename = "out.txt"
 	out = open(filename, 'w')
-	for k in result:
-		out.write(str(k) + " - " + str(result[k]) + '\n')
-		print(str(k) + " - " + str(result[k]))
+
+
+	# inserisci eventuali altri tasti non presenti nella lista
+	# -------------
+	out.write(match_unknown + t + str(unknown) + n)
+	out.write(match_del + t + str(_del) + n)
+	out.write(match_return + t + str(_return) + n)
+	out.write(match_right_cmd + t + str(right_cmd) + n)
+	out.write(match_left_cmd + t + str(left_cmd) + n)
+	out.write(match_left_shift + t + str(left_shift) + n)
+	out.write(match_left_option + t + str(left_option) + n)
+	out.write(match_tab + t + str(tab) + n)
+	out.write(match_up + t + str(up) + n)
+	out.write(match_down + t + str(down) + n)
+
+
+	# inserisci qui di seguito tutti i tasti che vuoi rilevare
+	# -------------
+	key = "abcdefghijklmnopqrstuvwxyz";
+	for a in key:
+	    out.write(a + t + str(txt.count(a)) + n)
+
 	out.close()
-	print('Output saved in ' + filename)
-	print('DONE')
-	exit(0)
+	print("done")
 
 main()
 
+
+""""
+
+apri il terminale e incolla la seguente stringa 
+
+python keyloggerParser.py
+
+
+
+"""
 
